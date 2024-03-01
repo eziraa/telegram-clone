75a8564b298d8ec6f94582f148c1337c2b4ba369
eziraa <ezratgab@gmail.com>
2024-03-01 11:24:19 +0300

    add container to channel list

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import Channel from "./Channel";
 // [calc(100% - 20rem)]
 export const Container = () => {
   return (
-    <div className="channel__container">
+    <div className="channel-container">
       <input
         type="text"
         className=" m-3 h-10 rounded-2xl w-11/12  inline-block"
