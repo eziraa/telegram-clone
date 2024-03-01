e9d88833644d2425b84a5a8b05ab76145c2c9df1
eziraa <ezratgab@gmail.com>
2024-03-01 11:26:19 +0300

    add icon class for folder component

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,6 +1,6 @@
 export const Folder = () => {
   return (
-    <div className="__icon">
+    <div className="icon">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path
           fill="ghostwhite"
