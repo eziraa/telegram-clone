233e99d9f9264c59b8c35355e0ac6b7e7e44d41e
eziraa <ezratgab@gmail.com>
2024-03-01 18:58:38 +0300

    styling hamburger and edit icon not to lost its shape

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -2,7 +2,12 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 const Hamburger = () => {
   return (
-    <div className="icon">
+    <div
+      className="icon"
+      style={{
+        marginTop: "15px",
+      }}
+    >
       <GiHamburgerMenu size={20} color={"#74728c"} />
     </div>
   );
