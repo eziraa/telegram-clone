f48827a1df6f422d44a1a15e6ea6e149ace26283
eziraa <ezratgab@gmail.com>
2024-03-02 14:31:46 +0300

    adding toggle state and toggle function to Main component to display and hidden the menu component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,14 @@
 import LeftSideBar from "./LeftSideBar";
 import { Container } from "./Container";
 import image from "../assets/images/back-1.png";
+import { Menu } from "./Menu";
+import { useState } from "react";
 const Main = () => {
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setDisplayMenu(!displayMenu);
+  };
   return (
     <div
       className="main"
@@ -9,7 +16,8 @@ const Main = () => {
         backgroundImage: `url(${image})`,
       }}
     >
-      <LeftSideBar />
+      <Menu displayMenu={displayMenu} toggleMenu={toggleMenu}/>
+      <LeftSideBar toggleMenu={toggleMenu} />
       <Container />
     </div>
   );
