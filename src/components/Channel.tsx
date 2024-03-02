695cf34d11f050854e9a7cef27854c837f6b81ef
eziraa <ezratgab@gmail.com>
2024-03-02 14:37:45 +0300

    adding state for channel component to make it active when it clicked

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { FaVolumeMute } from "react-icons/fa";
 import image1 from "../assets/images/profiles/image1.jpg";
 import image2 from "../assets/images/profiles/image2.jpg";
 import image3 from "../assets/images/profiles/image3.jpg";
@@ -5,16 +7,35 @@ import image4 from "../assets/images/profiles/image4.jpg";
 import image5 from "../assets/images/profiles/image5.jpg";
 const images = [image1, image2, image3, image4, image5];
 import channels from "./data/channels";
+
 const Channel = () => {
-  return channels.map((channel, index) => (
-    <div key={channel.id} className={`channel channel--${index}`}>
+  const [channelsList, setChannel] = useState(channels);
+  const [activeIndex, setActiveIndex] = useState(-1);
+
+  const makeActive = (index: number) => {
+    setActiveIndex(index);
+  };
+  return channelsList.map((channel, index) => (
+    <div
+      onClick={() => makeActive(index)}
+      key={channel.id}
+      className={`channel ${
+        index === activeIndex && "channel__active"
+      }  channel--${index}`}
+    >
       <img
         src={images[index >= 5 ? index - 5 : index >= 10 ? index - 10 : index]}
         alt=""
         className="channel__image"
       />
       <div className="channel__info ">
-        <p className="channel__text"> {channel.title} </p>
+        <div className="channel__header">
+          <p className="channel__text"> {channel.title} </p>
+          <div className="channel__mute">
+            <FaVolumeMute />
+          </div>
+          <p className="channel__created-at"> Dec 09:34 </p>
+        </div>
         <p className="channel__last-post">{channel.description}</p>
       </div>
     </div>
