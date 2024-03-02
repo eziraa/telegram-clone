696e321168971151b0b843a6ed06747042c3d125
eziraa <ezratgab@gmail.com>
2024-03-02 21:11:30 +0300

    modify the account component to insert profile image

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,10 +1,14 @@
+import image from "../assets/images/profiles/profile-1.png";
 interface Props {
   name: string;
 }
 export const Account = ({ name }: Props) => {
   return (
-    <div className="account">
-      <p className="account__user-name">{name}</p>
+    <div className="account" key={name}>
+      <div className="account__user">
+        <img className="account__user__image" src={image} />
+        <p className="account__user__name">{name}</p>
+      </div>
       <p className="account__notification-count">2</p>
     </div>
   );
