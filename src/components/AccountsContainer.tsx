a1d4e980911790bb0a6a3e148b55799443677dca
eziraa <ezratgab@gmail.com>
2024-03-02 21:13:27 +0300

    adding add account section

--- src/components/AccountsContainer.tsx
+++ src/components/AccountsContainer.tsx
@@ -1,4 +1,5 @@
 import { Account } from "./Account";
+import { IoAdd } from "react-icons/io5";
 
 export const AccountsContainer = () => {
   const accounts = ["Ezira", "Sutual"];
@@ -7,6 +8,14 @@ export const AccountsContainer = () => {
       {accounts.map((account) => (
         <Account name={account} />
       ))}
+      <div className="account" key={"op"}>
+        <div className="account__user">
+          <div className="account__user__image">
+            <IoAdd />
+          </div>
+          <div className="account__user__name">Add account</div>
+        </div>
+      </div>
     </div>
   );
 };
