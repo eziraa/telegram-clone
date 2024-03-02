c16034c865b9473728d7bdab2e768a9b2d225b00
eziraa <ezratgab@gmail.com>
2024-03-02 16:08:42 +0300

    adding profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,10 @@
+import image1 from "../assets/images/profiles/profile-1.png";
+export const Profile = () => {
+  return (
+    <div className="profile">
+      <img src={image1} alt="" className="profile__image" />
+      <p className="profile__user-name">Ezira</p>
+      <p className="profile__phone-number">+251 934 342 42</p>
+    </div>
+  );
+};
