201c342eacbaf55761c874c34729482bfd40552d
eziraa <ezratgab@gmail.com>
2024-03-03 21:49:10 +0300

    adding props to CreateChannel

--- src/components/CreateGroup.tsx
+++ src/components/CreateGroup.tsx
@@ -1,23 +1,35 @@
 import { FaCamera } from "react-icons/fa";
-
-export const CreateGroup = () => {
+import { Overlay } from "./Overlay";
+import { Fragment } from "react/jsx-runtime";
+interface Props {
+  displayItemIndex: number;
+  handleDisplayNone: () => void;
+}
+export const CreateGroup = ({ displayItemIndex, handleDisplayNone }: Props) => {
   return (
-    <div className="new-group">
-      <div className="upper">
-        <div className="new-group__image">
-          <FaCamera className="icon" color="white" />
+    <Fragment>
+      <Overlay display={displayItemIndex == 1} toggleMenu={handleDisplayNone} />
+      <div
+        className={`new-group ${displayItemIndex !== 1 && "new-group--hidden"}`}
+      >
+        <div className="upper">
+          <div className="new-group__image">
+            <FaCamera className="icon" color="white" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="group-name" className="new-group__name-label">
+              Group name
+            </label>
+            <input type="text" className="new-group__name-input" />
+          </div>
         </div>
-        <div className="input-field">
-          <label htmlFor="group-name" className="new-group__name-label">
-            Group name
-          </label>
-          <input type="text" className="new-group__name-input" />
+        <div className="lower">
+          <button className="btn btn-cancel" onClick={handleDisplayNone}>
+            Cancel
+          </button>
+          <button className="btn btn-next">Next</button>
         </div>
       </div>
-      <div className="lower">
-        <button className="btn btn-cancel">Cancel</button>
-        <button className="btn btn-next">Next</button>
-      </div>
-    </div>
+    </Fragment>
   );
 };
