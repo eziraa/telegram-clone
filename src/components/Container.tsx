be81a3e16df2db6a520d75644c7c44aa6b205cf1
eziraa <ezratgab@gmail.com>
2024-03-04 23:15:27 +0300

    change class name of the scrollbar to scrollbar

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,12 @@
 import Channel from "./Channel";
 import { Search } from "./Search";
-import "./new.css";
+import "./scroll-bar.css";
 // [calc(100% - 20rem)]
 export const Container = () => {
   return (
     <div className="body-container">
       <Search />
-      <div className="main-container">
+      <div className="scroll-container">
         <div className="chats-container">
           <Channel />
         </div>
