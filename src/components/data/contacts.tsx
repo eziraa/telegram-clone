a3a98e2a8a4e2d8b445c0316da48a3b1431a3294
eziraa <ezratgab@gmail.com>
2024-03-05 09:56:18 +0300

    removing list of contact objects from contacts and add to separate module called contacts to the data directory

---
+++ src/components/data/contacts.tsx
@@ -0,0 +1,185 @@
+import p1 from "../../assets/images/p1.jpg";
+import p2 from "../../assets/images/p2.jpg";
+import p3 from "../../assets/images/p3.jpg";
+import p4 from "../../assets/images/p4.jpg";
+import p5 from "../../assets/images/p5.jpg";
+import p6 from "../../assets/images/p6.jpg";
+import p7 from "../../assets/images/p7.jpg";
+import p8 from "../../assets/images/p8.jpg";
+import p9 from "../../assets/images/p9.jpg";
+import p10 from "../../assets/images/p10.jpg";
+const list_of_contacts = [
+  {
+    name: "Jack",
+    last_seen: "Last seen 3 minutes ago",
+    image: p1,
+  },
+
+  {
+    name: "Stephen",
+    last_seen: "Last seen 23 minutes ago",
+    image: p2,
+  },
+  {
+    name: "Solomon",
+    last_seen: "Last seen 10 minutes ago",
+    image: p3,
+  },
+  {
+    name: "Ezira",
+    last_seen: "Last seen 2 minutes ago",
+    image: p4,
+  },
+
+  {
+    name: "Ela Barra",
+    last_seen: "Last seen 3 hour ago",
+    image: p5,
+  },
+  {
+    name: "Ellis",
+    last_seen: "Last seen 1 hour ago",
+    image: p6,
+  },
+  {
+    name: "Ellis Barra",
+    last_seen: "Last seen 3 days ago",
+    image: p7,
+  },
+  {
+    name: "Stok",
+    last_seen: "Last seen with in a week",
+    image: p8,
+  },
+  {
+    name: "Erik",
+    last_seen: "Last seen with in a month",
+    image: p9,
+  },
+  {
+    name: "Gtag",
+    last_seen: "Last seen 3 minutes ago",
+    image: p10,
+  },
+  {
+    name: "Dirk",
+    last_seen: "Last seen 13 minutes ago",
+    image: p1,
+  },
+  {
+    name: "Jack",
+    last_seen: "Last seen 3 minutes ago",
+    image: p2,
+  },
+
+  {
+    name: "Stephen",
+    last_seen: "Last seen 23 minutes ago",
+    image: p3,
+  },
+  {
+    name: "Solomon",
+    last_seen: "Last seen 10 minutes ago",
+    image: p4,
+  },
+  {
+    name: "Ezira",
+    last_seen: "Last seen 2 minutes ago",
+    image: p1,
+  },
+
+  {
+    name: "Ela Barra",
+    last_seen: "Last seen 3 hour ago",
+    image: p5,
+  },
+  {
+    name: "Ellis",
+    last_seen: "Last seen 1 hour ago",
+    image: p6,
+  },
+  {
+    name: "Ellis Barra",
+    last_seen: "Last seen 3 days ago",
+    image: p7,
+  },
+  {
+    name: "Stok",
+    last_seen: "Last seen with in a week",
+    image: p8,
+  },
+  {
+    name: "Erik",
+    last_seen: "Last seen with in a month",
+    image: p9,
+  },
+  {
+    name: "Gtag",
+    last_seen: "Last seen 3 minutes ago",
+    image: p10,
+  },
+  {
+    name: "Dirk",
+    last_seen: "Last seen 13 minutes ago",
+    image: p1,
+  },
+  {
+    name: "Jack",
+    last_seen: "Last seen 3 minutes ago",
+    image: p1,
+  },
+
+  {
+    name: "Stephen",
+    last_seen: "Last seen 23 minutes ago",
+    image: p2,
+  },
+  {
+    name: "Solomon",
+    last_seen: "Last seen 10 minutes ago",
+    image: p3,
+  },
+  {
+    name: "Ezira",
+    last_seen: "Last seen 2 minutes ago",
+    image: p4,
+  },
+
+  {
+    name: "Ela Barra",
+    last_seen: "Last seen 3 hour ago",
+    image: p5,
+  },
+  {
+    name: "Ellis",
+    last_seen: "Last seen 1 hour ago",
+    image: p6,
+  },
+  {
+    name: "Ellis Barra",
+    last_seen: "Last seen 3 days ago",
+    image: p7,
+  },
+  {
+    name: "Stok",
+    last_seen: "Last seen with in a week",
+    image: p8,
+  },
+  {
+    name: "Erik",
+    last_seen: "Last seen with in a month",
+    image: p9,
+  },
+  {
+    name: "Gtag",
+    last_seen: "Last seen 3 minutes ago",
+    image: p10,
+  },
+  {
+    name: "Dirk",
+    last_seen: "Last seen 13 minutes ago",
+    image: p1,
+  },
+];
+
+export default list_of_contacts;
