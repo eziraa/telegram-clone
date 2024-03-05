2f759ad047ed4690790b1ce07af24cbc49626b74
eziraa <ezratgab@gmail.com>
2024-03-05 09:59:51 +0300

    adding more call objects

--- src/components/data/calls.tsx
+++ src/components/data/calls.tsx
@@ -245,6 +245,242 @@ const list_of_calls = [
     image: p8,
     type: "called",
   },
+  {
+    name: "Jack",
+    last_seen: "February 15, 2016 at 10:23 PM",
+    image: p1,
+    type: "called",
+  },
+
+  {
+    name: "Stephen",
+    last_seen: "February 25, 2016 at 00:23 PM",
+    image: p2,
+    type: "missed",
+  },
+  {
+    name: "Solomon",
+    last_seen: "February 1, 2016 at 10:53 AM",
+    image: p3,
+    type: "missed",
+  },
+  {
+    name: "Ezira",
+    last_seen: "January 15, 2016 at 04:23 PM",
+    image: p4,
+    type: "received",
+  },
+
+  {
+    name: "Ela Barra",
+    last_seen: "March 05, 2016 at 09:34 AM",
+    image: p5,
+    type: "called",
+  },
+  {
+    name: "Ellis",
+    last_seen: "February 25, 2016 at 04:23 PM",
+    image: p6,
+    type: "missed",
+  },
+  {
+    name: "Ellis Barra",
+    last_seen: "February 05, 2015 at 10:23 PM",
+    image: p7,
+    type: "called",
+  },
+  {
+    name: "Stok",
+    last_seen: "June 23, 2016 at 10:23 AM",
+    image: p8,
+    type: "missed",
+  },
+  {
+    name: "Erik",
+    last_seen: "July 05, 2016 at 06:23 AM",
+    image: p9,
+    type: "received",
+  },
+  {
+    name: "Gtag",
+    last_seen: "March 29, 2016 at 10:03 AM",
+    image: p10,
+    type: "received",
+  },
+  {
+    name: "Dirk",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p1,
+    type: "called",
+  },
+  {
+    name: "Jack",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p1,
+    type: "missed",
+  },
+
+  {
+    name: "Stephen",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p2,
+    type: "received",
+  },
+  {
+    name: "Solomon",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p3,
+    type: "called",
+  },
+  {
+    name: "Ezira",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p4,
+    type: "missed",
+  },
+
+  {
+    name: "Ela Barra",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p5,
+    type: "received",
+  },
+  {
+    name: "Ellis",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p6,
+    type: "missed",
+  },
+  {
+    name: "Ellis Barra",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p7,
+    type: "canceled",
+  },
+  {
+    name: "Stok",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p8,
+    type: "called",
+  },
+  {
+    name: "Jack",
+    last_seen: "February 15, 2016 at 10:23 PM",
+    image: p1,
+    type: "called",
+  },
+
+  {
+    name: "Stephen",
+    last_seen: "February 25, 2016 at 00:23 PM",
+    image: p2,
+    type: "missed",
+  },
+  {
+    name: "Solomon",
+    last_seen: "February 1, 2016 at 10:53 AM",
+    image: p3,
+    type: "missed",
+  },
+  {
+    name: "Ezira",
+    last_seen: "January 15, 2016 at 04:23 PM",
+    image: p4,
+    type: "received",
+  },
+
+  {
+    name: "Ela Barra",
+    last_seen: "March 05, 2016 at 09:34 AM",
+    image: p5,
+    type: "called",
+  },
+  {
+    name: "Ellis",
+    last_seen: "February 25, 2016 at 04:23 PM",
+    image: p6,
+    type: "missed",
+  },
+  {
+    name: "Ellis Barra",
+    last_seen: "February 05, 2015 at 10:23 PM",
+    image: p7,
+    type: "called",
+  },
+  {
+    name: "Stok",
+    last_seen: "June 23, 2016 at 10:23 AM",
+    image: p8,
+    type: "missed",
+  },
+  {
+    name: "Erik",
+    last_seen: "July 05, 2016 at 06:23 AM",
+    image: p9,
+    type: "received",
+  },
+  {
+    name: "Gtag",
+    last_seen: "March 29, 2016 at 10:03 AM",
+    image: p10,
+    type: "received",
+  },
+  {
+    name: "Dirk",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p1,
+    type: "called",
+  },
+  {
+    name: "Jack",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p1,
+    type: "missed",
+  },
+
+  {
+    name: "Stephen",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p2,
+    type: "received",
+  },
+  {
+    name: "Solomon",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p3,
+    type: "called",
+  },
+  {
+    name: "Ezira",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p4,
+    type: "missed",
+  },
+
+  {
+    name: "Ela Barra",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p5,
+    type: "received",
+  },
+  {
+    name: "Ellis",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p6,
+    type: "missed",
+  },
+  {
+    name: "Ellis Barra",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p7,
+    type: "canceled",
+  },
+  {
+    name: "Stok",
+    last_seen: "May 15, 2016 at 05:23 AM",
+    image: p8,
+    type: "called",
+  },
 ];
 
 export default list_of_calls;
