514bb5197ec305cda78945d62de39a8f1b70a739
eziraa <ezratgab@gmail.com>
2024-03-09 12:03:40 +0300

    adding scrollbar and icon class name to Icon components of CatSetting component

--- src/components/ChatSetting.tsx
+++ src/components/ChatSetting.tsx
@@ -24,93 +24,98 @@ export const ChatSetting = () => {
           onClick={() => display.handleDisplay(-2)}
         />
       </div>
-      <div className="section">
-        <div className="section__theme-setting">
-          <h3 className="header">Theme settings</h3>
-          <div className="section-item">
-            <IoBrushOutline />
-            <p className="name">Your name color</p>
-            <p className="value property">Ezira</p>
+      <div className="scroll-container">
+        <div className="section">
+          <div className="section__theme-setting">
+            <h3 className="header">Theme settings</h3>
+            <div className="section-item">
+              <IoBrushOutline className="icon" />
+              <p className="title">Your name color</p>
+              <p className="value property">Ezira</p>
+            </div>
+            <div className="section-item">
+              <MdNightlight className="icon" />
+              <p className="title">Auto-night mode</p>
+              <p className="value">Off</p>
+            </div>
           </div>
-          <div className="section-item">
-            <MdNightlight />
-            <p className="name">Auto-night mode</p>
-            <p className="value">Off</p>
+          <div className="section__custom-theme">
+            <h3 className="header">Custom themes</h3>
+            <div className="section-item">
+              <MdDryCleaning className="icon" />
+              <p className="title">Classic</p>
+            </div>
           </div>
-        </div>
-        <div className="section__custom_theme">
-          <h3 className="header">Custom themes</h3>
-          <div className="section-item">
-            <MdDryCleaning />
-            <p className="title">Classic</p>
+          <div className="section__wallpaper">
+            <h3 className="header">Chat wallpaper</h3>
+            <div className="section-item section-item--not">
+              <img src={img} alt="" className="image" />
+              <div className="choices">
+                <p className="choice">Choose from gallery</p>
+                <p className="choice">Choose from file</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="section__wallpaper">
-          <img src={img} alt="" className="image" />
-          <div className="section-item">
-            <p className="choice">Choose from gallery</p>
-            <p className="choice">Choose from file</p>
+          <div className="section__sticker">
+            <h3 className="header">Stickers and emoji</h3>
+            <div className="section-item">
+              <input type="checkbox" name="" id="" className="check" />
+              <label className="title">Large emoji</label>
+            </div>
+            <div className="section-item">
+              <input type="checkbox" name="" id="" className="check" />
+              <p className="title">Replace emoji automatically</p>
+            </div>
+            <div className="section-item">
+              <input type="checkbox" name="" id="" className="check" />
+              <p className="title">Suggest emoji replacements</p>
+            </div>
+            <div className="section-item">
+              <input type="checkbox" name="" id="" className="check" />
+              <p className="title">Suggest popular stickers by emoji</p>
+            </div>
+            <div className="section-item">
+              <input type="checkbox" name="" id="" className="check" />
+              <p className="title">Loop animated stickers</p>
+            </div>
+            <div className="section-item">
+              <BiSticker className="icon" />
+              <p className="title">Manage stickers sets</p>
+            </div>
+            <div className="section-item">
+              <BsEmojiExpressionless className="icon" />
+              <p className="title">Choose emoji set</p>
+            </div>
           </div>
-        </div>
-        <div className="section__sticker">
-          <h3 className="header">Stickers and emoji</h3>
-          <div className="section-item">
-            <Checkbox />
-            <p className="title">Large emoji</p>
+          <div className="section__message">
+            <h3 className="header">Messages</h3>
+            <div className="section-item">
+              <input type="radio" name="" id="" className="radio-btn" />
+              <p className="title">Send me with enter</p>
+            </div>
+            <div className="section-item">
+              <input type="radio" name="" id="" className="radio-btn" />
+              <p className="title">Send with ctrl + Enter</p>
+            </div>
+            <div className="section-item">
+              <input type="radio" name="" id="" className="radio-btn" />
+              <p className="title">Reply with double click</p>
+            </div>
+            <div className="section-item">
+              <input type="radio" name="" id="" className="radio-btn" />
+              <p className="title">Send reaction with double click</p>
+              <FaThumbsUp className="icon value" color="orange" />
+            </div>
+            <div className="section-item">
+              <input type="checkbox" name="" id="" className="check" />
+              <p className="title">Reaction button on messages</p>
+            </div>
           </div>
-          <div className="section-item">
-            <Check />
-            <p className="title">Replace emoji automatically</p>
-          </div>
-          <div className="section-item">
-            <Checkbox />
-            <p className="title">Suggest emoji replacements</p>
-          </div>
-          <div className="section-item">
-            <Checkbox />
-            <p className="title">Suggest popular stickers by emoji</p>
-          </div>
-          <div className="section-item">
-            <Checkbox />
-            <p className="title">Loop animated stickers</p>
-          </div>
-          <div className="section-item">
-            <BiSticker />
-            <p className="title">Manage stickers sets</p>
-          </div>
-          <div className="section-item">
-            <BsEmojiExpressionless />
-            <p className="title">Choose emoji set</p>
-          </div>
-        </div>
-        <div className="section__message">
-          <h3 className="header">Messages</h3>
-          <div className="section-item">
-            <RadioBtn />
-            <p className="title">Send me with enter</p>
-          </div>
-          <div className="section-item">
-            <RadioBtn />
-            <p className="title">Send with ctrl + Enter</p>
-          </div>
-          <div className="section-item">
-            <RadioBtn />
-            <p className="title">Reply with double click</p>
-          </div>
-          <div className="section-item">
-            <RadioBtn />
-            <p className="title">Send reaction with double click</p>
-            <FaThumbsUp className="icon" color="yellow" />
-          </div>
-          <div className="section-item">
-            <Checkbox />
-            <p className="title">Reaction button on messages</p>
-          </div>
-        </div>
-        <div className="section__archive">
-          <div className="section-item">
-            <BiArchiveIn />
-            <p className="title">Archive setting</p>
+          <div className="section__archive">
+            <div className="section-item">
+              <BiArchiveIn className="icon" />
+              <p className="title">Archive setting</p>
+            </div>
           </div>
         </div>
       </div>
