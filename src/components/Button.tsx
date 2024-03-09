ed38605b00a1b1c8ab125166075cbc3b780dce38
eziraa <ezratgab@gmail.com>
2024-03-09 22:29:55 +0300

    adding function to the props of Button to handle when it is clicked

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
-import React from "react";
 interface Props {
   text: string;
+  handleDisplay: () => void;
 }
-export const Button = ({ text }: Props) => {
-  return <button className="btn btn-next">{text} </button>;
+export const Button = ({ text, handleDisplay }: Props) => {
+  return (
+    <button className="btn btn-next" onClick={() => handleDisplay()}>
+      {text}{" "}
+    </button>
+  );
 };
