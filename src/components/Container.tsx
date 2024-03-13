1c833983e6de438e807992783cf415f7dad52141
eziraa <ezratgab@gmail.com>
2024-03-13 19:14:10 +0300

    Adding the MessageBoard component to the container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,20 @@
 import Channel from "./Channel";
+import MessageBoard from "./MessageBoard";
 import { Search } from "./Search";
+import chat from "../../public/Data/chat";
+
 export const Container = () => {
   return (
     <div className="body-container">
-      <Search />
-      <div className="scroll-container">
-        <div className="chats-container">
-          <Channel />
+      <div className="left-aside">
+        <Search />
+        <div className="scroll-container">
+          <div className="chats-container">
+            <Channel />
+          </div>
         </div>
       </div>
+      <MessageBoard chat={chat} />
     </div>
   );
 };
