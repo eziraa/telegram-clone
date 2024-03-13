2c98918ce7ac4163c9ad535d110ef4a30f15cef3
eziraa <ezratgab@gmail.com>
2024-03-13 19:12:40 +0300

    using contacts data from exported contact list of Data

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,12 +1,14 @@
 import { PiSortAscendingLight } from "react-icons/pi";
 import { Fragment } from "react/jsx-runtime";
 import { IoSearchOutline } from "react-icons/io5";
+import contacts from "../../public/Data/contacts";
 import list_of_contacts from "./data/contacts";
+
 import { useDisplayContext } from "../context/context";
-
-
+const contac = list_of_contacts;
 
 export const Contacts = () => {
+  const list_of_contacts = contacts;
   const display = useDisplayContext();
   return (
     <Fragment>
@@ -27,7 +29,7 @@ export const Contacts = () => {
           <p className="contacts__header__text">Contacts</p>
         </div>
         <div className="search">
-          <IoSearchOutline size={15} />
+          <IoSearchOutline className="icon" />
           <input
             type="text"
             className="search-input"
@@ -40,11 +42,18 @@ export const Contacts = () => {
               return (
                 <div className="contact" key={index}>
                   <div className="contact__profile">
-                    <img src={contact.image} alt="" className="image" />
+                    <img
+                      src={contac[index % contac.length].image}
+                      alt=""
+                      className="image"
+                    />
                   </div>
                   <div className="contact__info">
-                    <p className="contact__name"> {contact.name} </p>
-                    <p className="contact__last-seen"> {contact.last_seen} </p>
+                    <p className="contact__name">
+                      {" "}
+                      {contact.first_name} {contact.last_name}{" "}
+                    </p>
+                    <p className="contact__last-seen"> {contact.date} </p>
                   </div>
                 </div>
               );
