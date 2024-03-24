bedea366377d31ae8e95a19ede1a8536cc13d8d5
eziraa <ezratgab@gmail.com>
2024-03-24 19:45:37 +0300

    Adding back end

---
+++ backend/-- SQLite.sql
@@ -0,0 +1,8 @@
+-- SQLite
+INSERT INTO chat_channel (id, name, description, username)
+ VALUES(1, 'Learn React', 'This channel is to learn react from basic to advanced', 'learn_react'),
+ (2, 'Learn React', 'This channel is to learn react from basic to advanced', 'learn_react'),
+ (3, 'Learn React', 'This channel is to learn react from basic to advanced', 'learn_react'),
+ (4, 'Learn React', 'This channel is to learn react from basic to advanced', 'learn_react'),
+ (5, 'Learn React', 'This channel is to learn react from basic to advanced', 'learn_react'),
+ (6, 'Learn React', 'This channel is to learn react from basic to advanced', 'learn_react');
