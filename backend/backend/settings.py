363d9b5b9bf19bfdf6c36341ec047d5f0c54c019
eziraa <ezratgab@gmail.com>
2024-03-25 22:45:57 +0300

    Adding the cors allowed origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,7 +47,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chat',
     'rest_framework',
+    'corsheaders'
 ]
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
