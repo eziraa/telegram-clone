1dc8a76ea0ee90e6564d158cb52a677c56f4f4de
eziraa <ezratgab@gmail.com>
2024-03-25 22:49:33 +0300

    modifying the channellist and the channelDetail view to handle update and create also

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,21 +1,42 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import Channel
 from .serializers import ChannelSerializer
-
+from rest_framework import viewsets
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def ChannelList(request):
-    channels = Channel.objects.all()
-    serializer = ChannelSerializer(channels, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        channels = Channel.objects.all()
+        serializer = ChannelSerializer(
+            channels, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ChannelSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data)
 
 
-@api_view()
+@api_view('GET', 'PUT', 'DELETE')
 def ChannelDetail(request, id):
     channel = get_object_or_404(Channel, pk=id)
-    serializer = ChannelSerializer(channel)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ChannelSerializer(channel)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ChannelSerializer(channel, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        channel.delete()
+        return Response("Channel deleted", status.HTTP_204_NO_CONTENT)
+
+# class ChannelViewSet(viewsets.ModelViewSet):
+#     queryset = Channel.objects.all()
+#     serializer_class = ChannelSerializer
